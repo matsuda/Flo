73778f938811b5eec26ed1c20f18fa2fe5ad21a1
matsuda <kosukematsuda@gmail.com>
2017-05-31 15:58:52 +0900

    Draw graph

--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var graphView: GraphView!
     @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var averageWaterDrunk: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
 
     var isGraphViewShowing = false
 
@@ -44,11 +46,59 @@ class ViewController: UIViewController {
 
     @IBAction func counterViewTap(_ sender: UITapGestureRecognizer) {
         if isGraphViewShowing {
+            //hide Graph
             UIView.transition(from: graphView, to: counterView, duration: 1.0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
         } else {
+            //show Graph
+            setupGraphDisplay()
             UIView.transition(from: counterView, to: graphView, duration: 1.0, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
         }
         isGraphViewShowing = !isGraphViewShowing
+    }
+
+    func setupGraphDisplay() {
+        //Use 7 days for graph - can use any number,
+        //but labels and sample data are set up for 7 days
+        let noOfDays: Int = 7
+        
+        //1 - replace last day with today's actual data
+        graphView.graphPoints[graphView.graphPoints.count - 1] = counterView.counter
+        
+        //2 - indicate that the graph needs to be redrawn
+        graphView.setNeedsDisplay()
+        
+        maxLabel.text = "\(graphView.graphPoints.max()!)"
+        
+        //3 - calculate average from graphPoints
+        let average = graphView.graphPoints.reduce(0, +) / graphView.graphPoints.count
+        averageWaterDrunk.text = "\(average)"
+        
+        //set up labels
+        //day of week labels are set up in storyboard with tags
+        //today is last day of the array need to go backwards
+        
+        //4 - get today's day number
+        let dateFormatter = DateFormatter()
+        let calendar = Calendar.current
+        let componentOptions: Calendar.Component = .weekday
+//        let components = calendar.dateComponents([componentOptions], from: Date())
+//        var weekday = components.weekday
+        var weekday = calendar.component(componentOptions, from: Date())
+        let days = ["S", "S", "M", "T", "W", "T", "F"]
+        
+        //5 - set up the day name labels with correct day
+        for i in (1...days.count).reversed() {
+            if let labelView = graphView.viewWithTag(i) as? UILabel {
+                if weekday == 7 {
+                    weekday = 0
+                }
+                labelView.text = days[weekday]
+                weekday -= 1
+                if weekday < 0 {
+                    weekday = days.count - 1
+                }
+            }
+        }
     }
 }
 
